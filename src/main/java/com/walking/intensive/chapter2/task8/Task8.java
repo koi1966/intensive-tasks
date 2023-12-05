4d232d04907caaf2358c12b73aac309ba050367f
Oleg Klimchuk <koi196621@gmail.com>
2023-12-05 14:44:06 +0200

    chapter2.task8  ver 2 for study

--- src/main/java/com/walking/intensive/chapter2/task8/Task8.java
+++ src/main/java/com/walking/intensive/chapter2/task8/Task8.java
@@ -6,10 +6,42 @@ package com.walking.intensive.chapter2.task8;
 public class Task8 {
     public static void main(String[] args) {
 
+        System.out.printf("\nThe odds to get a happy ticket are %f", getHappyTicketChance(999_999));
+        //System.out.println(getCombinations(27, 3));
     }
 
-    static double getHappyTicketChance(int ticketNumber){
-        // Ваш код
-        return 0.0;
+    static double getHappyTicketChance(int ticketNumber) {
+        final int TRIAD_MAX_SUM = 27;
+        final int TRIAD_DIGITS_NUMBER = 3;
+        final int TRIAD_NUMBER = 2;
+        double happyTicketsNumber = 0;
+
+        for (int sum = 0; sum <= TRIAD_MAX_SUM / 2; sum++) {
+            happyTicketsNumber += Math.pow(getCombinations(sum, TRIAD_DIGITS_NUMBER), TRIAD_NUMBER);
+        }
+
+        return (happyTicketsNumber * 2) / 1_000_000;
+    }
+
+    static int getCombinations(int sum, int digitsNumber) {
+        int combinations = 0;
+
+        if (digitsNumber >= 1 && digitsNumber <= 3) {
+            digitsNumber--;
+
+            for (int digit = 0; digit <= 9; digit++) {
+                int remainderDigitsSum = sum - digit;
+
+                if (remainderDigitsSum == 0) {
+                    combinations++;
+                }
+
+                if (remainderDigitsSum > 0) {
+                    combinations += getCombinations(remainderDigitsSum, digitsNumber);
+                }
+            }
+        }
+
+        return combinations;
     }
 }
